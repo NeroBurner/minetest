3730e89b17e1af1f08b9e4335c73ccad5eff2357
Reinhold Gschweicher <pyro4hell@gmail.com>
2021-05-26 19:37:34 +0200

    CI: use IrllichtMt as subfolder for Linux builds

--- util/ci/common.sh
+++ util/ci/common.sh
@@ -13,8 +13,9 @@ install_linux_deps() {
 	else
 		# TODO: return old URL when IrrlichtMt 1.9.0mt2 is tagged
 		#wget "https://github.com/minetest/irrlicht/releases/download/1.9.0mt1/ubuntu-bionic.tar.gz"
-		wget "http://minetest.kitsunemimi.pw/irrlichtmt-patched-temporary.tgz" -O ubuntu-bionic.tar.gz
-		sudo tar -xaf ubuntu-bionic.tar.gz -C /usr/local
+		#wget "http://minetest.kitsunemimi.pw/irrlichtmt-patched-temporary.tgz" -O ubuntu-bionic.tar.gz
+		#sudo tar -xaf ubuntu-bionic.tar.gz -C /usr/local
+		git clone --depth 1 https://github.com/minetest/irrlicht.git lib/irrlichtmt
 	fi
 
 	sudo apt-get update
